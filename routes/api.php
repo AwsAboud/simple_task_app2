f639aa20849a9e4833780939bb9cd388b6e0e9ca
Aws <awsaboud7@gmail.com>
2025-05-14 15:41:33 +0300

    --amend

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\V1\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'v1','middleware' => 'api'], function () {
+    
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+
+        Route::group(['middleware' => 'auth:sanctum'], function () {
+            Route::post('logout', [AuthController::class, 'logout']);
+            Route::post('logout-all', [AuthController::class, 'logoutFromAllDevices']);
+        });
+    });
+
+    
+
+});
